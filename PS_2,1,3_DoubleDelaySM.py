c90bc1ffbb7f84707bc80d73cd07ca9703b0ffec
Alan Valdovinos <alanvaldo@hotmail.com>
2019-09-25 11:46:26 -0500

    Add files via upload

---
+++ PS_2,1,3_DoubleDelaySM.py
@@ -0,0 +1,42 @@
+class SM():
+    def start(self):
+        self.state = self.startState
+        
+    def step(self, inp):
+        (s, o) = self.getNextValues(self.state, inp)
+        self.state = s
+        return o
+    
+    def transduce(self, inputs):
+        self.start()
+        return [self.step(inp) for inp in inputs]
+        
+##################################
+# Double Delay SM
+##################################
+
+class Delay2Machine(SM):
+    def __init__(self, val0, val1):
+        self.startState = (val0, val1)
+        
+    def getNextValues(self, state, inp):
+        return ((state[1], inp), state[0])
+
+def runTestsDelay():
+    print 'Test1:', Delay2Machine(100, 10).transduce([1,0,2,0,0,3,0,0,0,4])
+    print 'Test2:', Delay2Machine(10, 100).transduce([0,0,0,0,0,0,1])
+    print 'Test3:', Delay2Machine(-1, 0).transduce([1,2,-3,1,2,-3])
+    # Test that self.state is not being changed.
+    m = Delay2Machine(100, 10)
+    m.start()
+    [m.getNextValues(m.state, i) for i in [-1,-2,-3,-4,-5,-6]]
+    print 'Test4:', [m.step(i) for i in [1,0,2,0,0,3,0,0,0,4]]
+
+# execute runTestsDelay() to carry out the testing, you should get:
+
+#Test1: [100, 10, 1, 0, 2, 0, 0, 3, 0, 0]
+#Test2: [10, 100, 0, 0, 0, 0, 0]
+#Test3: [-1, 0, 1, 2, -3, 1]
+#Test4: [100, 10, 1, 0, 2, 0, 0, 3, 0, 0]
+
+runTestsDelay()
---
+++ PS_2,1,4_CommentsMachine.py
@@ -0,0 +1,58 @@
+class SM():
+    def start(self):
+        self.state = self.startState
+        
+    def step(self, inp):
+        (s, o) = self.getNextValues(self.state, inp)
+        self.state = s
+        return o
+    
+    def transduce(self, inputs):
+        self.start()
+        return [self.step(inp) for inp in inputs]
+    
+##################################
+# Comments SM
+##################################
+
+x1 = '''def f(x):  # func
+   if x:   # test
+     # comment
+     return 'foo' '''
+
+x2 = '''#initial comment
+def f(x):  # func
+   if x:   # test
+     # comment
+     return 'foo' '''
+
+class CommentsSM(SM):
+    startState = False  # False when comment hasn't begun
+
+    def getNextValues(self, state, inp):
+        if inp == '#':
+            return (True, inp) # True when comment begins
+        elif inp != '\n' and state:
+            return (True, inp)
+        else:
+            return (False, None)
+ 
+def runTestsComm():
+    m = CommentsSM()
+    # Return only the outputs that are not None
+    print 'Test1:',  [c for c in CommentsSM().transduce(x1) if not c==None]
+    print 'Test2:',  [c for c in CommentsSM().transduce(x2) if not c==None]
+    # Test that self.state is not being changed.
+    m = CommentsSM()
+    m.start()
+    [m.getNextValues(m.state, i) for i in ' #foo #bar']
+    print 'Test3:', [c for c in [m.step(i) for i in x2] if not c==None]
+
+# execute runTestsComm() to carry out the testing, you should get:
+
+#Test1: ['#', ' ', 'f', 'u', 'n', 'c', '#', ' ', 't', 'e', 's', 't', '#', ' ', 'c', 'o', 'm', 'm', 'e', 'n', 't']
+#Test2: ['#', 'i', 'n', 'i', 't', 'i', 'a', 'l', ' ', 'c', 'o', 'm', 'm', 'e', 'n', 't', '#', ' ', 'f', 'u', 'n', 'c', '#', ' ', 't', 'e', 's', 't', '#', ' ', 'c', 'o', 'm', 'm', 'e', 'n', 't']
+#Test3: ['#', 'i', 'n', 'i', 't', 'i', 'a', 'l', ' ', 'c', 'o', 'm', 'm', 'e', 'n', 't', '#', ' ', 'f', 'u', 'n', 'c', '#', ' ', 't', 'e', 's', 't', '#', ' ', 'c', 'o', 'm', 'm', 'e', 'n', 't']
+
+runTestsComm()
+
